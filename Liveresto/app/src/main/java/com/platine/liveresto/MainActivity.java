d70d502e8b4df1285642e75caad51270fdeca646
radgeur <remy2112@hotmail.fr>
2017-01-18 12:34:05 +0100

    add the filter button in the actionBar and it works

--- Liveresto/app/src/main/java/com/platine/liveresto/MainActivity.java
+++ Liveresto/app/src/main/java/com/platine/liveresto/MainActivity.java
@@ -1,10 +1,17 @@
 package com.platine.liveresto;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int FILTRESCODE = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Context context = getApplicationContext();
+        switch (item.getItemId()) {
+            case R.id.filtres:
+                /*Intent filtres = new Intent(context, HomeActivity.class);
+                startActivityForResult(filtres, FILTRESCODE);*/
+                return true;
+            default :
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
